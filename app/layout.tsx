d10c028103aec29885718363bb59804da65e9d44
Alabi Temiloluwa <temiloluwaalabi33@gmail.com>
2023-08-21 14:51:58 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import Footer from '@/components/Footer'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Urbanist } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Leoshop Store',
+  description: 'Leoshop Store',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ModalProvider />
+        <ToastProvider />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
